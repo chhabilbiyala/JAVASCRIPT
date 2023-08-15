a8dbe127772ebacbc882dc036c70b81cc6e5d468
CHHABIL BIYALA <chhabilbiyala3444@gmail.com>
2023-08-15 07:01:03 +0530

    new files adding

--- #0.Anyfile.js
+++ #0.Anyfile.js
@@ -8,7 +8,7 @@ function calculateArea(radius) {
   return output;
 }
 console.log(calculateArea(radius));
-//-------------------------------------------
+
 function calculateCircumference(radius) {
   const output = [];
   for (i = 0; i < radius.length; i++) {
@@ -17,7 +17,7 @@ function calculateCircumference(radius) {
   return output;
 }
 console.log(calculateCircumference(radius));
-//-------------------------------------------
+
 function calculateDiameter(radius) {
   const output = [];
   for (i = 0; i < radius.length; i++) {
@@ -26,3 +26,31 @@ function calculateDiameter(radius) {
   return output;
 }
 console.log(calculateDiameter(radius));
+
+//------------------------------------------------
+
+const radius1 = [2, 4, 6, 8];
+
+const area = function(radius) {
+  return Math.PI * radius * radius;
+}
+
+const circumference = function(radius) {
+  return  2 * Math.PI * radius;
+}
+
+const diameter = function(radius) {
+  return 2 * radius;
+}
+
+const calculate = function (radius, logic) {
+  const output = [];
+  for (i = 0; i < radius.length; i++) {
+    output.push(logic(radius[i]));
+    return output;
+  }
+};
+
+console.log(calculate(radius, area));
+console.log(calculate(radius, circumference));
+console.log(calculate(radius, diameter));
