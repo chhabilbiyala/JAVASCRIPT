273c271e745ce7e89ffb86da16afa51dc092b6b1
CHHABIL BIYALA <chhabilbiyala3444@gmail.com>
2023-10-01 16:16:41 +0530

    new js files update

--- #0.Anyfile.js
+++ #0.Anyfile.js
@@ -1,8 +1,25 @@
-function logInfo(info) {
-    console.log(`${this.prefix}: ${info}`);
+const p1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("promise resolve value");
+    }, 10000);
+});
+
+const p2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("promise resolve value");
+  }, 5000);
+});
+
+async function handlePromise() {
+    console.log("Hello world");
+
+    const val = await p1;
+    console.log("Namaste js");
+    console.log(val);
+
+    const val2 = await p2;
+    console.log("Namaste js 2");
+    console.log(val2);
 }
 
-const logger = logInfo.bind({ prefix: '[INFO]' });
-
-logger('Data received');
-
+handlePromise();
